b2e61a1ea44a8188711c014461a73cf518eb6999
anthonykimani <anthonykimani@student.moringaschool.com>
2022-06-07 13:20:00 +0300

    Animates the Home section heading elements

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -8,7 +8,6 @@ const contactScroll = document.querySelector('.contactscroll');
 
 navButton.addEventListener('click',()=>{
     navMenu.classList.toggle("display");
-    console.log("hello");
 })
 
 contactButton.addEventListener('click',()=>{
@@ -17,28 +16,24 @@ contactButton.addEventListener('click',()=>{
 
 window.onload = function(){
     aboutScroll.addEventListener('click',function(){
-        console.log('hello');
          setTimeout(function (){
              window.scrollTo(0,800);
         },2);
     });
 
     skillScroll.addEventListener('click',function(){
-        console.log('hello');
          setTimeout(function (){
              window.scrollTo(0,1300);
         },2);
     })
 
     projectScroll.addEventListener('click',function(){
-        console.log('hello');
          setTimeout(function (){
              window.scrollTo(0,2200);
         },2);
     })
 
     contactScroll.addEventListener('click',function(){
-        console.log('hello');
          setTimeout(function (){
              window.scrollTo(0,3400);
         },2);
